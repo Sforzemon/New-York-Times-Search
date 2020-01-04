db4b6b5d978b3545e7ac6e689a2c1ae8dc658aa7
shelby palumbo <shelbypalumbo94@gmail.com>
2020-01-04 13:30:11 -0500

    updated script

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var key = vUleXwKENYlVeKyufEAPTl3QSJV7UvH1
- var queryURL =
-$.ajax({
+ var key = "key=vUleXwKENYlVeKyufEAPTl3QSJV7UvH1"
+ var search = "";
+ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q" + search + key;
+
+ $.ajax({
     url: queryURL,
     method: "GET"
   })
     .then(function(response) {
+    
+      $("#submit").on("click", function(){
+
+      $("seach-term").text();
+      $("start-year")
+      $("end-year")
+    })
+
+    });
