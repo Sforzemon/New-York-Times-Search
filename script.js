c7d0a6140a6ffae3e54e45d0706684abfe67ec12
shelby palumbo <shelbypalumbo94@gmail.com>
2020-01-05 19:35:08 -0500

    rearranged script

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
- var key = "key=vUleXwKENYlVeKyufEAPTl3QSJV7UvH1"
- var search = "";
+ var key = "&apikey=vUleXwKENYlVeKyufEAPTl3QSJV7UvH1"
+ var search = $("#search-term").val().trim();
  var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q" + search + key;
 
- $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-    
-      $("#submit").on("click", function(){
 
-      $("seach-term").text();
-      $("start-year")
-      $("end-year")
+      $("#submit").on("click", function(){
+        event.preventDefault();
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(response) {
+      
+ 
     })
 
     });
